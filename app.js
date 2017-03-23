f64a5236090d5900f9cd07623d2fbd9357e0b746
Adam Peryman <a.peryman@mighway.co>
2017-03-23 21:30:10 +1300

    More tinkering.

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Alex\'s mum is a lovely lady.')
+})
+
+app.listen(3000, () => {
+    console.log('We are listening on port 3000.') // eslint-disable-line no-console
+})
+
