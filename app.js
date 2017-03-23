4e9d4ccf10a9121d73ec2ac88855208295e79304
Adam Peryman <a.peryman@mighway.co>
2017-03-23 21:34:35 +1300

    Changed listen port.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.send('Alex\'s mum is a lovely lady.')
 })
 
-app.listen(3000, () => {
+app.listen(8080, () => {
     console.log('We are listening on port 3000.') // eslint-disable-line no-console
 })
 
