9929a8386b49c5ecb89d82fd9af62a6d39733f85
Adam Peryman <adam.peryman@gmail.com>
2017-03-24 09:09:17 +1300

    Oops.

--- server/index.js
+++ server/index.js
@@ -7,6 +7,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(8080, () => {
-    console.log('We are listening on port 3000.') // eslint-disable-line no-console
+    console.log('We are listening on port 8080.') // eslint-disable-line no-console
 })
-
