6f5e83befb37c004bcd497486f07318ea54fa415
Adam Peryman <a.peryman@mighway.co>
2017-04-02 21:14:09 +1200

    Now using winston!

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,14 @@
+import bodyParser from 'body-parser'
 import express from 'express'
+import morgan from 'morgan'
 import path from 'path'
 import webpack from 'webpack'
-import Log from 'log'
 
 import webpackDevConfig from '../../webpack.config.js'
+import { logger } from '../utils/logger'
 
 const app = express()
 const PORT = process.env.PORT || 8080
-const log = new Log('info')
 
 const indexPath = path.join(__dirname, '../public/index.html')
 const publicPath = express.static(path.join(__dirname, '../../build'))
@@ -15,6 +16,15 @@ const publicPath = express.static(path.join(__dirname, '../../build'))
 webpack(webpackDevConfig)
 
 app.use('/build', publicPath)
+
+app.use(morgan('combined', {
+  stream: logger.stream
+}))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.get('/', (req, res) => {
   res.sendFile(indexPath)
@@ -25,14 +35,14 @@ app.use((req, res) => {
 })
 
 app.listen(8080, () => {
-  log.info(`Now listening at http://localhost:${PORT}`)
+  logger.info(`Now listening at http://localhost:${PORT}`)
 })
 
 process.on('uncaughtException', error => {
-  log.error('Uncaught Exception: ', error)
+  logger.error('Uncaught Exception: ', error)
 })
 
 process.on('unhandledRejection', error => {
-  log.error('Unhandled Rejection: ', error)
+  logger.error('Unhandled Rejection: ', error)
 })
 
