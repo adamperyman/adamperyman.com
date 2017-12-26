0b7effea2b282fda7900166cd0a53df28c344be9
Adam Peryman <adam.peryman@gmail.com>
2017-12-27 03:16:19 +1300

    Boilerplate up and running.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,12 @@
-console.log('Hello, world!')
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+
+import 'css/style.css'
+
+const App = props => (
+  <div>Hello, world!</div>
+)
+
+export default App
+
+render(<App />, document.getElementById('app'))
